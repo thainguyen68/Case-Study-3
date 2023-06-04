3f852df4785b427d12a43895f477ea724c1003cd
thainguyen68 <nvt.689@gmail.com>
2023-06-04 12:58:22 +0700

    jsp

--- src/main/java/com/example/casestudy3/HelloServlet.java
+++ src/main/java/com/example/casestudy3/HelloServlet.java
@@ -4,24 +4,11 @@ import java.io.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
+@WebServlet(name = "helloServlet", value = "")
 public class HelloServlet extends HttpServlet {
-    private String message;
-
-    public void init() {
-        message = "Hello World!";
-    }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
-
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
+       response.sendRedirect("home.jsp");
     }
 
-    public void destroy() {
-    }
 }
